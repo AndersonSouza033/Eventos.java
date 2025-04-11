56231ee467635fb4837ccff57cbd30682cbd344a
AndersonSouza033 <andersonsouzapcb@gmail.com>
2025-04-11 15:30:13 -0300

    Update Participante.java

--- Participante.java
+++ Participante.java
@@ -4,46 +4,46 @@ import java.util.List;
 
 // Definindo meus atributos da classe dos participantes!
 public class Participante {
-    private String nomeParticipante;
-    private String emailParticipante;
-    private String senhaParticipante;
-    private String telefoneParticipante;
-    private int idadeParticipante;
-    private LocalDate nascimentoParticipante;
-    private List<Evento> eventoEscolhido;
+  private String nomeParticipante;
+  private String emailParticipante;
+  private String senhaParticipante;
+  private String telefoneParticipante;
+  private int idadeParticipante;
+  private LocalDate nascimentoParticipante;
+  private List<Evento> eventoEscolhido;
 
-    // Construtor do participante!
-    public Participante(String nomeParticipante, String emailParticipante, String senhaParticipante,
-        String telefoneParticipante, int idadeParticipante, LocalDate nascimentoParticipante, List<Evento> eventoEscolhido) {
-        this.nomeParticipante = nomeParticipante;
-        this.emailParticipante = emailParticipante;
-        this.senhaParticipante = senhaParticipante;
-        this.telefoneParticipante = telefoneParticipante;
-        this.idadeParticipante = idadeParticipante;
-        this.nascimentoParticipante = nascimentoParticipante;
-        this.eventoEscolhido = eventoEscolhido;
-    }
+  // Método construtor!
+  public Participante(String nomeParticipante, String emailParticipante, String senhaParticipante,
+    String telefoneParticipante, int idadeParticipante, LocalDate nascimentoParticipante, List<Evento> eventoEscolhido) {
+      this.nomeParticipante = nomeParticipante;
+      this.emailParticipante = emailParticipante;
+      this.senhaParticipante = senhaParticipante;
+      this.telefoneParticipante = telefoneParticipante;
+      this.idadeParticipante = idadeParticipante;
+      this.nascimentoParticipante = nascimentoParticipante;
+      this.eventoEscolhido = eventoEscolhido;
+  }
 
-    // Criando Getters (Ler)!
-    public String getSenha(){
-        return senhaParticipante;
-    }
+  // Criando Getters (Ler)!
+  public String getSenha(){
+    return senhaParticipante;
+  }
     
-    public String getEmail() {
-        return emailParticipante;
-    }
+  public String getEmail() {
+    return emailParticipante;
+  }
 
-    public String getNome() {
-        return nomeParticipante;
-    }
+  public String getNome() {
+    return nomeParticipante;
+  }
 
-    // Método para exibir os dados do participante!
-    @Override
-    public String toString() {
-        return "Nome: " + nomeParticipante +
-            "\nE-mail: " + emailParticipante +
-            "\nTelefone: " + telefoneParticipante +
-            "\nIdade: " + idadeParticipante +
-            "\nData de Nascimento: " + nascimentoParticipante;
+  // Método para exibir os dados do participante!
+  @Override
+  public String toString() {
+    return "Nome: " + nomeParticipante +
+      "\nE-mail: " + emailParticipante +
+      "\nTelefone: " + telefoneParticipante +
+      "\nIdade: " + idadeParticipante +
+      "\nData de Nascimento: " + nascimentoParticipante;
     }
 }
