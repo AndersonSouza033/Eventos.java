df22e21a3c92e07b59ff118b077adb1e45cd45c4
AndersonSouza033 <andersonsouzapcb@gmail.com>
2025-04-11 15:30:37 -0300

    Update Cantor.java

--- Cantor.java
+++ Cantor.java
@@ -1,25 +1,36 @@
 // Definindo meus atributos da classe dos cantores!
 public class Cantor{
-    private String nomeCantor;
-    private String emailCantor;
-    private String idadeCantor;
-    private String telefoneCantor;
-    private boolean ativoCantor;
+  private String nomeCantor;
+  private String emailCantor;
+  private String idadeCantor;
+  private String telefoneCantor;
+  private boolean ativoCantor;
     
-    public Cantor(String nomeCantor, String emailCantor, String idadeCantor, String telefoneCantor, boolean ativoCantor){
-        this.nomeCantor = nomeCantor;
-        this.emailCantor = emailCantor;
-        this.idadeCantor = idadeCantor;
-        this.telefoneCantor = telefoneCantor;
-        this.ativoCantor = true;
+    
+  // Método construtor!
+  public Cantor(String nomeCantor, String emailCantor, String idadeCantor, String telefoneCantor, boolean ativoCantor){
+    this.nomeCantor = nomeCantor;
+    this.emailCantor = emailCantor;
+    this.idadeCantor = idadeCantor;
+    this.telefoneCantor = telefoneCantor;
+    this.ativoCantor = true;
+  }
+    
+  // Método editar cantor!
+  public void editarCantor(String nomeCantor, String emailCantor, String idadeCantor, String telefoneCantor){
+    this.nomeCantor = nomeCantor;
+    this.emailCantor = emailCantor;
+    this.idadeCantor = idadeCantor;
+    this.telefoneCantor = telefoneCantor;
     }
-    @Override
-    public String toString() {
-        return "Nome: " + nomeCantor +
-            "\nE-mail: " + emailCantor +
-            "\nIdade: " + idadeCantor +
-            "\nTelefone: " + telefoneCantor +
-            "\nAtivo: " + ativoCantor ;       
+      
+  // Método para listar!
+  @Override
+  public String toString() {
+    return "Nome: " + nomeCantor +
+      "\nE-mail: " + emailCantor +
+      "\nIdade: " + idadeCantor +
+      "\nTelefone: " + telefoneCantor +
+      "\nAtivo: " + ativoCantor ;       
     }
 }
-
